061fcf6f165cd5a870c90fd413bee1eeffc995c6
nearxu <2448895924@qq.com>
2019-12-11 13:44:13 +0800

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import SortComponent from './sortable';
+// import SortComponent from './sortable';
+import EditorDemo from './wiki';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <SortComponent />
+      <EditorDemo />
     </div>
   );
 }
